5012977abd7c4805a59da2c9f27c06c4714a1ad0
Einar Huseby <einar.huseby@gmail.com>
2020-01-04 19:30:47 +0100

    Show sourcelink

--- docs-source/conf.py
+++ docs-source/conf.py
@@ -82,7 +82,7 @@ pygments_style = None
 html_theme = 'sphinx_rtd_theme'
 
 html_copy_source = False
-html_show_sourcelink = False
+html_show_sourcelink = True
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
